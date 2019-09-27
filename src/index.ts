567fa3a063796c637c1eb7ac7364f139a86ad81b
bodi44 <stogrinec.bodia@gmail.com>
2019-09-27 18:07:21 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['laskdjf'] }));
+app.use(router);
+
+app.listen(3000, () =>
+  console.log('app listening on port 3000'),
+);
